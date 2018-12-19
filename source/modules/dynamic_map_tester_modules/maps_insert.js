a6b25844008c00e578cbe8cb2f4ba3a36db37ad0
Omar Morales <omarmorales@google.com>
2018-12-19 13:17:58 -0600

    Correct messy push

--- source/modules/dynamic_map_tester_modules/maps_insert.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const mapsJS = document.createElement('script');
-mapsJS.src = 'https://maps.googleapis.com/maps/api/js?callback=google_maps_init&key=${api_key}';
-document.getElementsByTagName('head')[0].appendChild(mapsJS);
-
-window.addEventListener('insert' mapsJS);
