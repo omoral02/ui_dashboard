2b4ded19a2af2b7c4c56c290ec75db992c35f019
Omar Morales <omarmorales@google.com>
2019-05-30 18:17:48 -0500

    udpate app.js file

--- dist/app.js
+++ dist/app.js
@@ -9,28 +9,6 @@ let indexRouter = require('./routes/index');
 let adminRouter = require('./routes/admin');
 
 const filePath = path.resolve(__dirname, '..', 'public/');
-const options = {
-  title: 'title test',
-  message: 'message test'
-};
-let cb = ((err)=>{
-  console.log(err);
-});
-
-app.engine('html', function (filePath, options, cb) { // define the template engine
-  fs.readFile(filePath, function (err, content) {
-    if (err) {return cb(err);}
-    // this is an extremely simple template engine
-    let rendered = content.toString().replace('<title></title>', '<title>' + options.title + '</title>')
-    .replace('<h1></h1>', '<h1>' + options.message + '</h1>').
-    replace('<link>', '<link rel=\'stylesheet\' src=\'./public/css/index.css\'>');
-    return cb(null, rendered);
-  })
-});
-
-// pug view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -45,11 +23,11 @@ app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/app.js
+++ src/app.js
@@ -7,30 +7,36 @@ let fs = require('fs');
 let app = express();
 let indexRouter = require('./routes/index');
 let adminRouter = require('./routes/admin');
-
 const filePath = path.resolve(__dirname, '..', 'public/');
-const options = {
-  title: 'title test',
-  message: 'message test'
-};
-let cb = ((err)=>{
-  console.log(err);
-});
 
-app.engine('html', function (filePath, options, cb) { // define the template engine
-  fs.readFile(filePath, function (err, content) {
-    if (err) {return cb(err);}
-    // this is an extremely simple template engine
-    let rendered = content.toString().replace('<title></title>', '<title>' + options.title + '</title>')
-    .replace('<h1></h1>', '<h1>' + options.message + '</h1>').
-    replace('<link>', '<link rel=\'stylesheet\' src=\'./public/css/index.css\'>');
-    return cb(null, rendered);
-  })
-});
+//// Simple but manual template engine//// 
+//**
+// const options = {
+//   title: 'title test',
+//   message: 'message test'
+// };
+// let cb = ((err)=>{
+//   console.log(err);
+// });
+// app.engine('html', function (filePath, options, cb) { // define the template engine
+//   fs.readFile(filePath, function (err, content) {
+//     if (err) {return cb(err);}
+//     // this is an extremely simple template engine
+//     let rendered = content.toString().replace('<title></title>', '<title>' + options.title + '</title>')
+//     .replace('<h1></h1>', '<h1>' + options.message + '</h1>').
+//     replace('<link>', '<link rel=\'stylesheet\' src=\'./public/css/index.css\'>');
+//     return cb(null, rendered);
+//   })
+// });
+//  **/
 
+//// Pug template engine ////
+//** 
 // pug view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
+// **/
+
 
 app.use(logger('dev'));
 app.use(express.json());
