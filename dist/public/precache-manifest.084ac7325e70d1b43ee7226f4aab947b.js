6d28f43336d05ab64827e0b996b012fd7a5e58d1
Omar Morales <omarmorales@google.com>
2020-03-16 11:29:39 -0500

    set base for new fork

---
+++ dist/public/precache-manifest.084ac7325e70d1b43ee7226f4aab947b.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/js/main.dd1a3690a5a71124557a.js"
+  },
+  {
+    "url": "/js/runtime.dd1a3690a5a71124557a.js"
+  },
+  {
+    "revision": "98cacca93dfbf83fe10a",
+    "url": "/main.0.css"
+  }
+]);
